25a2ee369c1e56ce717276c4372cc34d8ab89fcc
Huseyin Eskan <huseyineskan01@gmail.com>
2024-11-06 22:57:13 +0100

    added deleteCocktail and you can delete them individually or in bulk

--- src/components/Myfavorites.jsx
+++ src/components/Myfavorites.jsx
@@ -10,35 +10,79 @@ function MyFavorites() {
   const getMyFavorites = async () => {
     const data = await axios.get("http://localhost:3000/myfavorites");
     setAllMyFavoritesData(data.data);
-    console.log(data.data);
+  };
+
+  const deleteCocktail = async (deleteId = false) => {
+    if (deleteId) {
+      await axios.delete(`http://localhost:3000/myfavorites/${deleteId}`);
+      console.log("deleted one");
+    } else {
+      try {
+        await allMyFavoritesData.map(async (cocktail) => {
+          await axios.delete(
+            `http://localhost:3000/myfavorites/${cocktail.id}`
+          );
+          console.log("Deleted all");
+        });
+      } catch (error) {
+        console.log(`Error :`, error);
+      }
+    }
   };
 
   useEffect(() => {
     getMyFavorites();
-  }, []);
+  }, [deleteCocktail]);
 
   return (
     <div className="container all-past-cocktails-container">
-      <h2>My Favorites Cocktails</h2>
+      <h2>
+        My Favorites Cocktails
+        <div className="delete-all-cocktails" onClick={() => deleteCocktail()}>
+          Clear entire list
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z" />
+          </svg>
+        </div>
+      </h2>
       <div className="my-cocktails-box">
         {allMyFavoritesData ? (
-          allMyFavoritesData.map((drink, i) => (
-            <div key={i} className="my-coctail-box">
-              <a href="#">
-                <LazyLoadImage
-                  src={drink.img}
-                  alt={drink.name}
-                  effect="blur"
-                  wrapperProps={{
-                    style: { transitionDelay: "1s" },
+          allMyFavoritesData.length > 0 ? (
+            allMyFavoritesData.map((drink, i) => (
+              <div key={i} className="my-coctail-box">
+                <div
+                  className="delete-cocktail"
+                  onClick={() => {
+                    deleteCocktail(drink.id);
                   }}
-                />
-                <p>{drink.name}</p>
-              </a>
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                    <path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" />
+                  </svg>
+                </div>
+                <a href="#">
+                  <LazyLoadImage
+                    src={drink.img}
+                    alt={drink.name}
+                    effect="blur"
+                    wrapperProps={{
+                      style: { transitionDelay: "1s" },
+                    }}
+                  />
+                  <p>{drink.name}</p>
+                </a>
+              </div>
+            ))
+          ) : (
+            <div className="basic-message">
+              <p>Not found coctail.</p>
+              <a href="/">Go to Get A Drink!</a>
             </div>
-          ))
+          )
         ) : (
-          <p>loading...</p>
+          <div className="basic-message">
+            <p>loading...</p>
+          </div>
         )}
       </div>
     </div>
